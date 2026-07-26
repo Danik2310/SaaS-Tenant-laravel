732c2115b1519c5d5791f60876fe0d870d1baa88
Danik2310 <daniel30512676@gmail.com>
2026-07-26 14:29:11 -0500

    fix(tenants): add missing toast import in TenantForm

--- resources/js/modules/tenants/TenantForm.jsx
+++ resources/js/modules/tenants/TenantForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { toast } from 'sonner';
 import { FormCard, FormInput, ButtonPrimary, ButtonSecondary, FormActions } from '@/Components/FormElements';
 import api from '../../services/api';
 
