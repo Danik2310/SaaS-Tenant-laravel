ace8be994feff3325abe31696b3fdcd447047b82
Danik2310 <daniel30512676@gmail.com>
2026-07-26 14:30:57 -0500

    chore: remove unused dead code files

--- resources/js/Layouts/LandlordLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LandlordLayout({ children }) {
-    return (
-        <div style={{ display: 'flex', minHeight: '100vh' }}>
-            {children}
-        </div>
-    );
-}
